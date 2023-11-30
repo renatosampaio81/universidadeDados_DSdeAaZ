fbda4e376f73deb873b09ba6f1d592ec095ca6d3
Renato Sampaio <renatosampaio81@yahoo.com.br>
2023-11-30 16:15:01 -0300

    Chapter 07 - Identifying anomaly in data

--- SSMS/SQLQuery1.sql
+++ SSMS/SQLQuery1.sql
@@ -10,8 +10,8 @@ SELECT TOP (1000) [idCliente]
       ,[Age]
       ,[Date Joined]
       ,[Balance]
-  FROM [DS_Trainning].[dbo].[RAW_ClientesBanco_20210423]
+  FROM [DS_Trainning].[dbo].[RAW_ClientesBanco_20231125]
 
-  select count(*) from [DS_Trainning].[dbo].[RAW_ClientesBanco_20210423]
+  select count(*) from [DS_Trainning].[dbo].[RAW_ClientesBanco_20231130]
 
   delete from [DS_Trainning].[dbo].[RAW_ClientesBanco_20210423]
